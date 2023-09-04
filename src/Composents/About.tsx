4508cbdd5451e9fc5ab1c5976a7af7eb4d721a4b
Pierre Dok <pierredok@outlook.fr>
2023-09-04 15:26:20 +0200

    transparent scrollbar

--- src/Composents/About.tsx
+++ src/Composents/About.tsx
@@ -1,6 +1,6 @@
 
 
-import React, { FunctionComponent, useState, useRef} from 'react'
+import React, { FunctionComponent, useState, useRef, useEffect} from 'react'
 import DOK from "../Assets/desktop/Dok1.svg"
 import AboutImg from "../Assets/desktop/InternetPage.svg"
 import MobileDOK from "../Assets/mobile/DOK.svg"
@@ -8,7 +8,7 @@ import MobileAbout from "../Assets/mobile/internet-Page.svg"
 import "./About.css"
 import { motion, useScroll, useTransform, useSpring } from 'framer-motion'
 import { useMediaQuery } from 'react-responsive'
-import { useNavigate, Navigate } from 'react-router-dom'
+import { redirect } from 'react-router-dom'
 import { LoadingBar } from '../Composents/Loading';
 
 
@@ -25,6 +25,15 @@ export const About: FunctionComponent = () => {
     const isMobile = useMediaQuery({
         query: "(max-device-width: 768px)",
       });
+
+    const [isLoadingComplete, setIsLoadingComplete] = useState(false)
+  
+    const handleLoadingPage = () => {
+        setIsLoadingComplete(true);
+    }
+
+    const transition = useTransform(scrollYProgress,[0, 0.97, 1], [false, false, true])
+
 
 
 
@@ -56,7 +65,7 @@ export const About: FunctionComponent = () => {
 
 
         // Use Framer Motion function for design the Mobile animation of the about Page
-        const mobileScaleY1 = useTransform(smoothScroll, [0, 0.2, 0.3, 0.4], [2, 5.5, 2, 0])
+        const mobileScaleY1 = useTransform(smoothScroll, [0, 0.2, 0.3, 0.4], [2, 3.8, 2, 0])
         const mobileScaleY2 = useTransform(smoothScroll, [0.22, 0.3, 0.5, 0.8], [0, 1, 1 , 1])
         const mobileScaleY3 = useTransform(smoothScroll, [0.45, 0.6, 0.85, 0.95], [0, -2, -5.8, -1])
 
@@ -66,16 +75,11 @@ export const About: FunctionComponent = () => {
         const mobileOpacity2 = useTransform(smoothScroll, [0.22, 0.8, 0.81], [1, 1, 0])
         const mobileOpacity3 = useTransform(smoothScroll, [0,0.45, 0.7, 1, 1.5], [1, 1, 1, 1 ,1])
 
-        const mobileTranslateY1 = useTransform(smoothScroll, [0, 0.2,0.4], ['70vh', '2.5vh' ,'2.5vh']);
+        const mobileTranslateY1 = useTransform(smoothScroll, [0, 0.2,0.4], ['33vh', '1vh' ,'1vh']);
         const mobileTranslateY2 = useTransform(smoothScroll, [0.22, 0.3 , 0.45, 0.6, 0.9], ['90vh', '37vh' , '2.5vh', '-30vh', '-150vh']);
         const mobileTranslateY3 = useTransform(smoothScroll, [0.5, 0.85,  0.95], ['99vh', '98vh', '17.5vh']);
 
 
-        const [isLoadingComplete, setIsLoadingComplete] = useState(false)
-  
-        const handleLoadingPage = () => {
-          setIsLoadingComplete(true);
-        }
 
         if(isLoadingComplete === false){
 
---
+++ src/Composents/Transition.tsx
@@ -0,0 +1,12 @@
+import React, {FunctionComponent} from 'react'
+
+
+export const Transition: FunctionComponent = () => {
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
