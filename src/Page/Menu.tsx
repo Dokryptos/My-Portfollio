7052836483c619b1e130fb1a5d429ff29acc4a2b
Pierre Dok <pierredok@outlook.fr>
2023-09-11 17:48:04 +0200

    Fix animation menu

--- src/Page/Menu.tsx
+++ src/Page/Menu.tsx
@@ -1,5 +1,5 @@
 
-import React,{useState, FunctionComponent, ImgHTMLAttributes} from 'react'
+import React,{useState, FunctionComponent, useEffect} from 'react'
 import './Menu.css'
 import Navbar from '../Composents/Navbar'
 import { Header } from '../Composents/Header'
@@ -13,6 +13,8 @@ export const Menu: FunctionComponent = () => {
     let [backgroundColor, setBackgroundColor] = useState<string>('#252527')
     let [imageBoolean, setImageBoolean] = useState<boolean>(false)
     let [imageMenu, setImageMenu] = useState<string | undefined>(Menu1)
+    let [isImgVisible, setIsImgVisible] = useState<boolean>(false) 
+    let [isLiVisible, setIsLiVisible] = useState<boolean>(false) 
 
     const handleLinkClick = (color: string) => {
         setBackgroundColor(color);
@@ -35,31 +37,85 @@ export const Menu: FunctionComponent = () => {
     }
 
 
+    useEffect(() => {
+        const timeOutImg = setTimeout(() => {
+            setIsImgVisible(true);
+        }, 1500);
+
+        const timeOutLi = setTimeout(() => {
+            setIsLiVisible(true)
+        }, 1000)
+
+        return () => {
+            clearTimeout(timeOutImg);
+            clearTimeout(timeOutLi);
+        };
+    })
+
+    const liVariants = {
+        hidden: {translateX: '38vw', translateY: '25vh'},
+        visible: { translateX: '10vw', translateY: '25vh'},
+    }
+
+    const imgVariants = {
+        hidden: { opacity: 0, translateY: '25vh'},
+        visible: { opacity: 1, translateY: '25vh'},
+    };
+
     return(
         <div className='menu-container' style={{ backgroundColor: backgroundColor}}>
             <Header onColorChange={handleLinkClick} />
             <Navbar imageUrl={imageBoolean} />
-            <motion.div className='projet'>
-                <ul className='list-project'>
-                    <motion.li 
-                    initial={{translateX: '50vw', translateY: '30vh'}}
-                    animate={{ translateX: '10vw', translateY: '30vh'}}
-                    transition={{ duration: 1, ease: [0, 0, 0.5, 1]}}
-                    onClick={() => {handleChangeImg(1)
-                    }}>Chef on the Way</motion.li>
+            <div className='projet'>
+                <ul className={`list-project`}>
 
-                    <li onClick={() => {handleChangeImg(2)}}>Events App</li>
-                    <li onClick={() => {handleChangeImg(3)}}>Zombie Survival</li>
-                    <li onClick={() => {handleChangeImg(4) }}>Lapicide</li>
+                    <motion.li 
+                    initial='hidden'
+                    animate={isLiVisible ? 'visible' : 'hidden'}      
+                    variants={liVariants}
+                    transition={{ duration: 0.5, ease: [0, 0, 0.5, 1]}}
+                    onClick={() => {handleChangeImg(1)}}>
+                        Chef on the Way</motion.li>
+
+
+                    <motion.li 
+                    initial='hidden'
+                    animate={isLiVisible ? 'visible' : 'hidden'}      
+                    variants={liVariants}
+                    transition={{ duration: 0.75, ease: [0, 0, 0.5, 1]}} 
+                    onClick={() => {handleChangeImg(2)}}>
+                        Events App</motion.li>
+                    
+
+                    <motion.li 
+                    initial='hidden'
+                    animate={isLiVisible ? 'visible' : 'hidden'}      
+                    variants={liVariants}                    
+                    transition={{ duration: 1, ease: [0, 0, 0.5, 1]}}
+                    onClick={() => {handleChangeImg(3)}}>
+                        Zombie Survival</motion.li>
+                    
+
+                    <motion.li 
+                    initial='hidden'
+                    animate={isLiVisible ? 'visible' : 'hidden'}      
+                    variants={liVariants}
+                    transition={{ duration: 1.25, ease: [0, 0, 0.5, 1]}} 
+                    onClick={() => {handleChangeImg(4) }}>
+                        Lapicide</motion.li>
+
                 </ul>
-            </motion.div >
+
             <motion.div
-            initial={{opacity: 0}}
-            animate={{opacity:1}}
-            transition={{ duration: 2}}
+            initial='hidden'
+            animate={isImgVisible ? 'visible' : 'hidden'}
+            variants={imgVariants}
+            transition={{ duration: 0.5}}
+            
             >
-                <img src={imageMenu} alt='imgMenu'  />
+                <img src={imageMenu} alt='imgMenu' className="img-menu" />
             </motion.div>
-        </div>
+        </div >
+    </div>
     )
 }
