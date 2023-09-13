5227092e7efd21c894cf79930bcd42aae013b7b0
Pierre Dok <pierredok@outlook.fr>
2023-09-13 18:25:04 +0200

    Add commentary on the Menu page and fix background color Li when is selected

--- src/Page/Menu.tsx
+++ src/Page/Menu.tsx
@@ -13,13 +13,22 @@ import { Link } from 'react-router-dom'
 
 export const Menu: FunctionComponent = () => {
 
+    // Sent color to the Header for the Color fonction
     let [backgroundColor, setBackgroundColor] = useState<string>('#252527')
+    // Sent color oh the Image for the Logo color function
     let [imageBoolean, setImageBoolean] = useState<boolean>(false)
+
+    // Set up for the selection of project with link, image and Back ground Color
     let [imageMenu, setImageMenu] = useState<string | undefined>(ChefOnTheWay)
-    let [isImgVisible, setIsImgVisible] = useState<boolean>(false) 
-    let [isLiVisible, setIsLiVisible] = useState<boolean>(false) 
+    let [isSelected, setIsSelected] = useState<number>(0)
     let [link, setLink] = useState<string>('https://chef-ontheway.netlify.app')
 
+    // Set up timer for Starter animation
+    let [isImgVisible, setIsImgVisible] = useState<boolean>(false) 
+    let [isLiVisible, setIsLiVisible] = useState<boolean>(false) 
+
+
+    // Set up for Navbar and Header color Function
     const handleLinkClick = (color: string) => {
         setBackgroundColor(color);
         setImageBoolean(true);
@@ -27,31 +36,31 @@ export const Menu: FunctionComponent = () => {
 
 
 
-    {/*
-    https://event-app.adaptable.app/
-    https://lapicide.netlify.app/
-
-*/}
+    // Onclick function for the Menu Page with Image, link Background color
     const handleChangeImg = (value:number) => {
         if(value === 1){
             return (
                 setImageMenu(ChefOnTheWay),
-                setLink('https://chef-ontheway.netlify.app')
+                setLink('https://chef-ontheway.netlify.app'),
+                setIsSelected(1)
                 )
         } else if(value === 2){
             return(
                 setImageMenu(EventApp),
-                setLink('https://event-app.adaptable.app')
+                setLink('https://event-app.adaptable.app'),
+                setIsSelected(2)
             )
         }else if(value === 3){
             return (
                 setImageMenu(ZombieSurvival),
-                setLink('https://dokryptos.github.io/FinalTest-Survival_Game/index.html')
+                setLink('https://dokryptos.github.io/FinalTest-Survival_Game/index.html'),
+                setIsSelected(3)
             )
         }else if (value === 4){
             return (
                 setImageMenu(Lapicide),
-                setLink('https://lapicide.netlify.app')
+                setLink('https://lapicide.netlify.app'),
+                setIsSelected(4)
             )
         } else {
             return (
@@ -63,6 +72,7 @@ export const Menu: FunctionComponent = () => {
     }
 
 
+    // Timer when you coming inside this page for animation
     useEffect(() => {
         const timeOutImg = setTimeout(() => {
             setIsImgVisible(true);
@@ -78,20 +88,27 @@ export const Menu: FunctionComponent = () => {
         };
     })
 
+
+
+    // Variants for Motion framer Li animation
     const liVariants = {
         hidden: {translateX: '38vw', translateY: '25vh'},
         visible: { translateX: '10vw', translateY: '25vh'},
     }
-
     const imgVariants = {
         hidden: { opacity: 0, translateY: '25vh'},
         visible: { opacity: 1, translateY: '25vh'},
     };
 
+
+
     return(
         <div className='menu-container' style={{ backgroundColor: backgroundColor}}>
+            
             <Header onColorChange={handleLinkClick} />
             <Navbar imageUrl={imageBoolean} />
+
+
             <div className='projet'>
                 <ul className={`list-project`}>
 
@@ -100,6 +117,7 @@ export const Menu: FunctionComponent = () => {
                     animate={isLiVisible ? 'visible' : 'hidden'}      
                     variants={liVariants}
                     transition={{ type:'spring', duration:0.75}}
+                    className={isSelected === 1 ? 'selected' : 'not'}
                     onClick={() => {handleChangeImg(1)}}>
                         Chef on the Way</motion.li>
 
@@ -108,7 +126,8 @@ export const Menu: FunctionComponent = () => {
                     initial='hidden'
                     animate={isLiVisible ? 'visible' : 'hidden'}      
                     variants={liVariants}
-                    transition={{ type:'spring', duration: 1.25}} 
+                    transition={{ type:'spring', duration: 1.25}}
+                    className={isSelected === 2 ? 'selected' : 'not'}
                     onClick={() => {handleChangeImg(2)}}>
                         Events App</motion.li>
                     
@@ -118,6 +137,7 @@ export const Menu: FunctionComponent = () => {
                     animate={isLiVisible ? 'visible' : 'hidden'}      
                     variants={liVariants}                    
                     transition={{ type:'spring', duration: 1.75}}
+                    className={isSelected === 3 ? 'selected' : 'not'}
                     onClick={() => {handleChangeImg(3)}}>
                         Zombie Survival</motion.li>
                     
@@ -127,6 +147,7 @@ export const Menu: FunctionComponent = () => {
                     animate={isLiVisible ? 'visible' : 'hidden'}      
                     variants={liVariants}
                     transition={{ type:'spring', duration: 2.5}} 
+                    className={isSelected === 4 ? 'selected' : 'not'}
                     onClick={() => {handleChangeImg(4) }}>
                         Lapicide</motion.li>
 
